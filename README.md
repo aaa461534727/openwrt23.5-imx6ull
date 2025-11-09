34e72c3bc5376cf2a75c553bfbd8015e705f149a
dengbaowen <461534727@qq.com>
2025-11-09 02:55:51 -0800

    初始源码，支持imx6ull正点原子核心板v1.6、底板v2.2,支持编译框架

---
+++ README.md
@@ -0,0 +1,14 @@
+# 基于imx6ull的OpenWrt开发
+
+1. [快速上手](doc/quick_start_guide.md)
+
+2. [烧录镜像到 SD卡](doc/burn_sd_image.md)
+
+3. [基于 SD卡 烧录 eMMC 固件](doc/burn_emmc_image.md)
+
+4. [OpenWRT 使用 EC20 无线网卡模块上网](doc/ec20_network.md)
+
+5. [有线网络和 4G 网络切换](doc/eth_4g_auto_switching.md)
+
+1. [常见问题](doc/faq.md)
+
---
+++ target/linux/imx/image/cortexa7.mk
@@ -0,0 +1,44 @@
+DEVICE_VARS += UBOOT
+
+include common.mk
+
+define Device/Default
+  PROFILES := Default
+  FILESYSTEMS := squashfs ext4
+  KERNEL_INSTALL := 1
+  KERNEL_SUFFIX := -uImage
+  KERNEL_NAME := zImage
+  KERNEL := kernel-bin | uImage none
+  KERNEL_LOADADDR := 0x80008000
+  IMAGES :=
+endef
+
+define Device/technexion_imx7d-pico-pi
+  DEVICE_VENDOR := TechNexion
+  DEVICE_MODEL := PICO-PI-IMX7D
+  UBOOT := pico-pi-imx7d
+  DEVICE_DTS := imx7d-pico-pi
+  DEVICE_PACKAGES := kmod-sound-core kmod-sound-soc-imx kmod-sound-soc-imx-sgtl5000 \
+	kmod-can kmod-can-flexcan kmod-can-raw kmod-leds-gpio \
+	kmod-input-touchscreen-edt-ft5x06 kmod-usb-hid kmod-btsdio \
+	kmod-brcmfmac brcmfmac-firmware-4339-sdio cypress-nvram-4339-sdio
+  FILESYSTEMS := squashfs
+  IMAGES := sysupgrade.bin
+  IMAGE/combined.bin := append-rootfs | pad-extra 128k | imx-sdcard-raw-uboot
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += technexion_imx7d-pico-pi
+
+define Device/imx6ull-14x14-emmc
+  DEVICE_VENDOR := Alientek
+  DEVICE_MODEL := imx6ull-14x14-emmc
+  DEVICE_DTS := imx6ull-14x14-emmc-none
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb3 kmod-usb-ohci kmod-usb-uhci \
+		kmod-usb-wdm kmod-usb-core kmod-usb-hid kmod-usb-net \
+		kmod-usb-net-cdc-ether kmod-usb-net-cdc-mbim kmod-usb-net-cdc-subset
+  FILESYSTEMS := squashfs
+  IMAGES := sysupgrade.bin
+  IMAGE/combined.bin := append-rootfs | pad-extra 128k | imx6ull-sdcard-raw-uboot
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += imx6ull-14x14-emmc
